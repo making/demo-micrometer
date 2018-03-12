e480d6c4be56fb17f151238011bd2c5ddd958ee9
Toshiaki Maki <tmaki@pivotal.io>
2018-03-13 02:10:42 +0900

    Init

---
+++ src/main/java/com/example/demomicrometer/DemoMicrometerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demomicrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicrometerApplication.class, args);
+	}
+}
