091afb7067ae000ed28418b228ac5ebdb508d4f3
Toshiaki Maki <tmaki@pivotal.io>
2018-03-13 22:57:23 +0900

    Add Datadog

--- src/main/java/com/example/demomicrometer/DemoMicrometerApplication.java
+++ src/main/java/com/example/demomicrometer/DemoMicrometerApplication.java
@@ -1,12 +1,28 @@
 package com.example.demomicrometer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class DemoMicrometerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoMicrometerApplication.class, args);
+    }
+
+    @Bean
+    @Profile("cloud")
+    public MeterRegistryCustomizer meterRegistryCustomizer(
+            @Value("${vcap.application.name:}") String applicationName,
+            @Value("${vcap.application.instance_id:}") String instanceId
+    ) {
+        return registry -> registry.config().commonTags(
+                "cf_app_name", applicationName, //
+                "cf_app_instance_id", instanceId //
+        );
     }
 }
