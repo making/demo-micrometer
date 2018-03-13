361964659ce49bb1c58915902d2f13f32fbdbf6b
Toshiaki Maki <tmaki@pivotal.io>
2018-03-14 00:51:45 +0900

    Enable Eureka

--- src/main/java/com/example/demomicrometer/DemoMicrometerApplication.java
+++ src/main/java/com/example/demomicrometer/DemoMicrometerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoMicrometerApplication {
 
     public static void main(String[] args) {
